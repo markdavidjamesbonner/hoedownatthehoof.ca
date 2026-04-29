5b4d13f4f92441fc48a28660f6cff4b6ed0ec97d
mark bonner <realmarkbonner@gmail.com>
2026-04-29 06:31:14 -0400

    flowstate / supabase stuff removed

--- src/assets/events _ Table Editor _ flowstate _ markdavidjamesbonner's Org _ Supabase_files/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^\\/dashboard(?:\\/(_next\\/data\\/[^/]{1,}))?\\/api(?:\\/((?:[^\\/#\\?]+?)(?:\\/(?:[^\\/#\\?]+?))*))?(\\.json)?[\\/#\\?]?$",
-    "originalSource": "/api/:function*"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
